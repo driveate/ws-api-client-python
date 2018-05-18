ab837a693c0cc8643f96ba5a206bc1fc65ff0a8d
Pavel Ulyashev <p.ulyashev@gmail.com>
2018-05-18 12:26:33 +0600

    Bump version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "ws-api-client-python"
-VERSION = "1.0.0"
+VERSION = "1.0.2"
 # To install the library, run the following
 #
 # python setup.py install
